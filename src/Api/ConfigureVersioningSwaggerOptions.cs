238256a0cf46214f761baac6dc14d3ef39bf5346
Alexey Burlakov <lexiphanx@gmail.com>
2023-05-24 23:37:16 +0400

    Configure, add Swagger UI and versioning

---
+++ src/Api/ConfigureVersioningSwaggerOptions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Greentube.DemoWallet.Api;
+
+/// <summary>
+/// The class which registers all IConfigureOptions necessary for versioning in Swagger.
+/// </summary>
+internal class ConfigureVersioningSwaggerOptions :
+    IConfigureOptions<SwaggerGenOptions>,
+    IConfigureOptions<SwaggerUIOptions>
+{
+    private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, new() { Version = description.GroupName, Title = "Wallet API" });
+        }
+    }
+
+    public void Configure(SwaggerUIOptions options)
+    {
+        var apiVersionDescriptions = _apiVersionDescriptionProvider.ApiVersionDescriptions;
+
+        // Reverse order of version - to show the latest version first
+        for (var i = apiVersionDescriptions.Count - 1; i >= 0; i--)
+        {
+            var description = apiVersionDescriptions[i];
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"Wallet API {description.GroupName}");
+        }
+    }
+
+    public ConfigureVersioningSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+    {
+        _apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,43 @@
+using System.Reflection;
+using Greentube.DemoWallet.Api;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// This file is added to .gitignore and .dockerignore files,
+// thus every developer will have chance to set his own configuration for any testing purposes
+builder.Configuration.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: false);
+
+// --------------------- Configure services -------------------------
+
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options => options.DefaultApiVersion = new ApiVersion(1, 0));
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV"; // Major, optional minor version, and suffix: 1-rc -> 1-rc, 1.1 -> 1.1
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddSwaggerGen(opts => opts.IncludeXmlComments(Path.ChangeExtension(Assembly.GetExecutingAssembly().Location, "xml"), true));
+builder.Services.ConfigureOptions<ConfigureVersioningSwaggerOptions>();
+
+// ---------------- Configure application pipeline ------------------
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+
+// Basic endpoints which can be used for health nad deployment checks
+var thisAssemblyName = Assembly.GetExecutingAssembly().GetName();
+app.MapGet("/", () => thisAssemblyName.Name);
+app.MapGet("/version", () => thisAssemblyName.Version?.ToString());
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapFallback(async (HttpContext httpContext, CancellationToken ct) =>
+{
+    httpContext.Response.StatusCode = 404;
+    await httpContext.Response.WriteAsync("404 Not Found", ct);
+});
 
 app.Run();
